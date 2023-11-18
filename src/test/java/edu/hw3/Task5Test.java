5175c09ced6bd73181ab4a1f04670c42769293e3
Masha <Alice@gmail.com>
2023-11-18 16:55:22 +0300

    Исправлены недочеты

--- src/test/java/edu/hw3/Task5Test.java
+++ src/test/java/edu/hw3/Task5Test.java
@@ -18,7 +18,7 @@ public class Task5Test {
         test1.add("David Hume");
         test1.add("Rene Descartes");
         assertThat(edu.hw3.Task5.parseContacts(test1, "ASC").toString()).isEqualTo(
-            "[lastNameFirstName[firstName=Thomas, lastName=Aquinas], lastNameFirstName[firstName=Rene, lastName=Descartes], lastNameFirstName[firstName=David, lastName=Hume], lastNameFirstName[firstName=John, lastName=Locke]]");
+            "[LastNameFirstName[firstName=Thomas, lastName=Aquinas], LastNameFirstName[firstName=Rene, lastName=Descartes], LastNameFirstName[firstName=David, lastName=Hume], LastNameFirstName[firstName=John, lastName=Locke]]");
 
         ArrayList<String> test2 = new ArrayList<>();
         test2.add("Thomas Aquinas");
@@ -26,7 +26,7 @@ public class Task5Test {
         test2.add("David Hume");
         test2.add("Rene Descartes");
         assertThat(edu.hw3.Task5.parseContacts(test2, "DESC").toString()).isEqualTo(
-            "[lastNameFirstName[firstName=John, lastName=Locke], lastNameFirstName[firstName=David, lastName=Hume], lastNameFirstName[firstName=Rene, lastName=Descartes], lastNameFirstName[firstName=Thomas, lastName=Aquinas]]");
+            "[LastNameFirstName[firstName=John, lastName=Locke], LastNameFirstName[firstName=David, lastName=Hume], LastNameFirstName[firstName=Rene, lastName=Descartes], LastNameFirstName[firstName=Thomas, lastName=Aquinas]]");
 
         ArrayList<String> test3 = new ArrayList<>();
         test3.add("A BBB");
@@ -34,7 +34,7 @@ public class Task5Test {
         test3.add("C");
         test3.add("DD DD");
         assertThat(edu.hw3.Task5.parseContacts(test3, "ASC").toString()).isEqualTo(
-            "[lastNameFirstName[firstName=BBB, lastName=A], lastNameFirstName[firstName=A, lastName=BBB], lastNameFirstName[firstName=C, lastName=null], lastNameFirstName[firstName=DD, lastName=DD]]");
+            "[LastNameFirstName[firstName=BBB, lastName=A], LastNameFirstName[firstName=A, lastName=BBB], LastNameFirstName[firstName=C, lastName=null], LastNameFirstName[firstName=DD, lastName=DD]]");
 
         ArrayList<String> test4 = new ArrayList<>();
         test4.add("A BBB");
@@ -42,6 +42,6 @@ public class Task5Test {
         test4.add("C");
         test4.add("DD DD");
         assertThat(edu.hw3.Task5.parseContacts(test4, "DESC").toString()).isEqualTo(
-            "[lastNameFirstName[firstName=DD, lastName=DD], lastNameFirstName[firstName=C, lastName=null], lastNameFirstName[firstName=A, lastName=BBB], lastNameFirstName[firstName=BBB, lastName=A]]");
+            "[LastNameFirstName[firstName=DD, lastName=DD], LastNameFirstName[firstName=C, lastName=null], LastNameFirstName[firstName=A, lastName=BBB], LastNameFirstName[firstName=BBB, lastName=A]]");
     }
 }
